224b00b3af21e57fc811057012f124ecfd8c9478
Yan <mm7246591@yahoo.com.tw>
2021-11-16 16:08:47 +0800

    11/16

--- Yan/Valid-Parentheses.js
+++ Yan/Valid-Parentheses.js
@@ -1,31 +1,33 @@
-/**
- * 1.use loop check '(','{','[' 
- * @param {string} s
- * @return {boolean}
- */
-const isValid = s => {
-    for (let i = 0; i < s.length; i++) {
-        for (let j = i + 1; j < s.length; j++) {
-            switch (s[i]) {
-                case '(':
-                    if (s[j] === ')') {
-                        return true;
-                    } else {
-                        return false;
-                    }
-                case '{':
-                    if (s[j] === '}') {
-                        return true;
-                    } else {
-                        return false;
-                    }
-                case '[':
-                    if (s[j] === ']') {
-                        return true;
-                    } else {
-                        return false;
-                    }
+(function() {
+    /**
+     * 1.use loop check '(','{','[' 
+     * @param {string} s
+     * @return {boolean}
+     */
+    const isValid = s => {
+        for (let i = 0; i < s.length; i++) {
+            for (let j = i + 1; j < s.length; j++) {
+                switch (s[i]) {
+                    case '(':
+                        if (s[j] === ')') {
+                            return true;
+                        } else {
+                            return false;
+                        }
+                    case '{':
+                        if (s[j] === '}') {
+                            return true;
+                        } else {
+                            return false;
+                        }
+                    case '[':
+                        if (s[j] === ']') {
+                            return true;
+                        } else {
+                            return false;
+                        }
+                }
             }
         }
-    }
-};
+    };
+})()
